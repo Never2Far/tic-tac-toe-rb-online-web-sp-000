95e812290f5d2d6af5886a5e2db6044b9430527d
Seth Near <seth.j.near@gmail.com>
2020-06-11 05:37:06 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,86 @@
+WIN_COMBINATIONS = [
+  [0,1,2],  # Top row
+  [3,4,5],  # Middle row (horizontal)
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Left Column
+  [1,4,7],  # Middle Column
+  [2,5,8],  # Right Column
+  [0,4,8],  # Diagonal 1 (negative slope)
+  [2,4,6]   # Diagonal 2 (positive slope)
+]
+
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+  return index
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+
+def won?(board)
+
+  exes = board.each_index.select{ |i| board[i] == "X"}
+  ohs = board.each_index.select{ |i| board[i] == "O"}
+
+  experms = exes.permutation(3).to_a
+  ohperms = ohs.permutation(3).to_a
+  x_winning_index = WIN_COMBINATIONS.detect{ |win_combo| experms.include?(win_combo) }
+  o_winning_index = WIN_COMBINATIONS.detect{ |win_combo| ohperms.include?(win_combo) }
+
+
+  if (!!x_winning_index && !!o_winning_index)
+    return false
+  elsif x_winning_index
+       return x_winning_index
+  elsif o_winning_index
+       return o_winning_index
+    end
+  end
+
+def full?(board)
+return board.none? {|i| (i == " ") || (i == "")}
+end
+
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+if full?(board) || won?(board) || draw?(board)
+  return true
+else
+  return false
+end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  #elsif won?(board)[0] == "O"
+    #return "O"
+  #elsif won?(board)[0] == "X"
+else
+    return nil
+  end
+end
